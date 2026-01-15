80518edec31cc7f86cc1338dce3b247ad6ecb40d
Giovanni-Romuald <giovannikamdem7@gmail.com>
2026-01-15 19:38:35 +0100

    v8

--- bzmarket/settings.py
+++ bzmarket/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!8g058$$$e&dd8u^d4d#q-*h5mu=2(ha(k-k5*sl+dita!7sq#'
+SECRET_KEY ='django-insecure-!8g058$$$e&dd8u^d4d#q-*h5mu=2(ha(k-k5*sl+dita!7sq#'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -74,13 +78,23 @@ WSGI_APPLICATION = 'bzmarket.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+# Forcer SSL Railway / Neon / Supabase
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require'
+}
+
+
 
 
 # Password validation
@@ -121,7 +135,10 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'client.Utilisateur'
 
-STATIC_FILES_DIRS = [
-    BASE_DIR / "static"
+# Répertoire où Django cherchera les fichiers statiques des apps
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # ton dossier global "static" à la racine du projet
 ]
 
+# Répertoire où collectstatic copiera tous les fichiers statiques pour production
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
